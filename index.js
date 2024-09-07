efb46a8614a43d9be201897122d43a7d69e68ebe
Uhri Kenji Canave <uhrikenji@gmail.com>
2024-09-07 22:53:55 +0800

    forgot to remove some stuff

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const studentRoute = require("./routes/studentroutes.js");
-const productRoute = require("./routes/productroutes.js");
-const userRoute = require("./routes/userroutes.js");
-const orderRoute = require("./routes/orderroutes.js");
 const app = express();
 
 //import for model schema
-const Product = require("./models/product.model.js");
 const Student = require("./models/student.model.js");
-const User = require("./models/user.model.js");
-const Order = require("./models/order.model.js");
 
 //middleware to use json
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //routes
-app.use("/api/products", productRoute);
 app.use("/api/students", studentRoute);
-app.use("/api/users", userRoute);
-app.use("/api/orders", orderRoute);
 
 // GET request
 app.get("/", function (req, res) {
